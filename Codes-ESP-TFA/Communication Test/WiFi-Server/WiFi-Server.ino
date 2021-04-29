d0df039f73a3850f8dc52083ee7cb0bf632240dc
rptshri <rptshrivastava@gmai.com>
2021-04-29 10:27:48 +0530

    Marwan Code Completed

--- Codes-ESP-TFA/Communication Test/WiFi-Server/WiFi-Server.ino
+++ Codes-ESP-TFA/Communication Test/WiFi-Server/WiFi-Server.ino
@@ -1,4 +1,4 @@
-//https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html
+// https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html
 // play with MDNS at last
 
 #include <ESP8266WiFi.h>
@@ -8,6 +8,9 @@
 #include <ESP8266WebServer.h> // Include the WebServer library
 
 #include <Arduino_JSON.h>
+
+char ClientToken[] = "wfsnmyto-n47b-6bhc-7fam-4ltzatyixcmyg5";
+String LastApiKey = "";
 
 /* Put your SSID & Password */
 const char *ssid = "NodeMCU";      // Enter SSID here
@@ -20,9 +23,13 @@ IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
 
+unsigned long previousKeyCreationTime = 0;
+const unsigned int KeyValidityInterval = 120000;
+
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("\n\n");
 
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
@@ -30,12 +37,13 @@ void setup()
   Serial.print("IP address:\t");
   Serial.println(WiFi.softAPIP()); // Send the IP address of the ESP8266 to the computer
 
-  server.on("/", HTTP_POST, handle_OnConnect);
-  server.on("/login", HTTP_POST, handleLogin);
+  server.on("/data", HTTP_POST, handle_OnData);
+  server.on("/login", HTTP_POST, handle_Login);
   server.onNotFound(handle_NotFound);
 
   server.begin();
   Serial.println("HTTP server started");
+  //  generateRandomKey();
 }
 void loop()
 {
@@ -43,51 +51,9 @@ void loop()
   MDNS.update();
 }
 
-void handle_OnConnect()
-{
-  Serial.println("Connection Request!");
-
-  if (server.hasArg("plain") == false)
-  { //Check if body received
-    server.send(200, "text/plain", "Body not received");
-    return;
-  }
-  String message = "Body received:\n";
-  message += server.arg("plain");
-  message += "\n";
-  Serial.println(message);
-
-  JSONVar incomingMessage = JSON.parse(server.arg("plain"));
-  if (JSON.typeof(incomingMessage) == "undefined")
-  {
-    Serial.println("Parsing input failed!");
-    return;
-  }
-
-  Serial.print("JSON.typeof(incomingMessage) = ");
-  Serial.println(JSON.typeof(incomingMessage)); // prints: object
-
-  if (incomingMessage.hasOwnProperty("hi"))
-  {
-    Serial.print("incomingMessage[\"hi\"] = ");
-    Serial.println(incomingMessage["hi"]);
-  }
-  else
-  {
-    Serial.println("No hi");
-  }
-
-  JSONVar myObject;
-  myObject["hello"] = "world";
-  myObject["true"] = true;
-  myObject["x"] = 42;
-  String jsonString = JSON.stringify(myObject);
-  server.send(200, "application/json", jsonString);
-}
-
-void handleLogin()
+void handle_Login()
 { // If a POST request is made to URI /login
-
+  Serial.println();
   Serial.println("*******Login Request!*******");
 
   if (server.hasArg("plain") == false)
@@ -108,40 +74,144 @@ void handleLogin()
   {
     Serial.println("Parsing input failed!");
     JSONVar myObject;
-    myObject["Error"] = "Invalid Json";
+    myObject["Error"] = "Not Acceptable";
     String jsonString = JSON.stringify(myObject);
-    server.send(400, "application/json", jsonString); // The request is invalid, so send HTTP status 400
-
+    server.send(406, "application/json", jsonString); // The request is invalid, so send HTTP status 400
     return;
   }
-
   if (!incomingMessage.hasOwnProperty("username") || !incomingMessage.hasOwnProperty("password") || incomingMessage["username"] == NULL || incomingMessage["password"] == NULL)
   {
     Serial.println("Invalid Key & Values!");
     JSONVar myObject;
-    myObject["Error"] = "Invalid key & Values";
+    myObject["Error"] = "Non-Authoritative Information";
     String jsonString = JSON.stringify(myObject);
-    server.send(400, "application/json", jsonString); // The request is invalid, so send HTTP status 400
+    server.send(203, "application/json", jsonString); // The request is invalid, so send HTTP status 400
+    return;
+  }
+  if (JSON.stringify(incomingMessage["username"]) == "\"admin\"" && JSON.stringify(incomingMessage["password"]) == "\"password123\"")
+  { // If both the username and the password are correct
+    String Token = JSON.stringify(incomingMessage["Token"]);
+    Token.remove(0, 1);
+    Token.remove(38, 1);
+    if (Token == ClientToken)
+    {
+      Serial.println("Initial Token validated | Sending API Key");
+      JSONVar myObject;
+      LastApiKey = generateRandomKey();
+      myObject["ApiKey"] = LastApiKey;
+      String jsonString = JSON.stringify(myObject);
+      server.send(202, "application/json", jsonString);
+      previousKeyCreationTime = millis(); //save the time of Api Creation
+      Serial.print("previousKeyCreationTime:  ");
+      Serial.println(previousKeyCreationTime);
+    }
+    else
+    {
+      JSONVar myObject;
+      myObject["Status"] = "Accepted but wrong Token Key";
+      String jsonString = JSON.stringify(myObject);
+      server.send(202, "application/json", jsonString);
+    }
+  }
+  else
+  {
+    JSONVar myObject;
+    myObject["Error"] = "Bad Request";
+    String jsonString = JSON.stringify(myObject); // Username and password don't match
+    server.send(400, "application/json", jsonString);
+  }
+}
+
+void handle_OnData()
+{
+  Serial.println();
+  Serial.println("*******Data Receive!*******");
+  Serial.print("Last ApiKey Valid till:  ");
+  int timeRemaining = KeyValidityInterval - (millis() - previousKeyCreationTime);
+  Serial.println(timeRemaining);
+
+  if (timeRemaining < 0)
+  {
+    JSONVar myObject;
+    myObject["Status"] = "Proxy Authentication Required";
+    String jsonString = JSON.stringify(myObject);
+    server.send(407, "application/json", jsonString);
+  }
+
+  if (server.hasArg("plain") == false)
+  { //Check if body received
+    Serial.println("No Body Received");
+    JSONVar myObject;
+    myObject["Error"] = "No Body Received";
+    String jsonString = JSON.stringify(myObject);
+    server.send(400, "application/json", jsonString);
+    return;
+  }
+  String message = "Body received: ";
+  message += server.arg("plain");
+  Serial.println(message);
+
+  JSONVar incomingMessage = JSON.parse(server.arg("plain"));
+  if (!incomingMessage.hasOwnProperty("ApiKey") || !incomingMessage.hasOwnProperty("data") || incomingMessage["ApiKey"] == NULL || incomingMessage["data"] == NULL)
+  {
+    Serial.println("Invalid Key & Values!");
+    JSONVar myObject;
+    myObject["Error"] = "Non-Authoritative Information";
+    String jsonString = JSON.stringify(myObject);
+    server.send(203, "application/json", jsonString); // The request is invalid, so send HTTP status 400
     return;
   }
 
-  if (JSON.stringify(incomingMessage["username"]) == "\"admin\"" && JSON.stringify(incomingMessage["password"]) == "\"password123\"")
-  { // If both the username and the password are correct
+  String ClientApiKey = JSON.stringify(incomingMessage["ApiKey"]);
+  ClientApiKey.remove(0, 1);
+  ClientApiKey.remove(38, 1);
+  if (ClientApiKey == LastApiKey)
+  {
+    Serial.println("ApiKey validated | Saving your data!");
     JSONVar myObject;
-    myObject["Status"] = "Logged In Successfully!";
+    myObject["Status"] = "Data Received!";
     String jsonString = JSON.stringify(myObject);
     server.send(201, "application/json", jsonString);
   }
   else
   {
+    Serial.println("ApiKey validated | Saving your data!");
     JSONVar myObject;
-    myObject["Error"] = "Unauthorized";
-    String jsonString = JSON.stringify(myObject); // Username and password don't match
-    server.send(401, "application/json", jsonString);
+    myObject["Status"] = "Non-Authoritative Information!";
+    String jsonString = JSON.stringify(myObject);
+    server.send(203, "application/json", jsonString);
   }
 }
 
 void handle_NotFound()
 {
   server.send(404, "text/plain", "Not found");
+}
+
+String RandomKey = "";
+String generateRandomKey()
+{
+  // dc52bc78-8011-47e6-ab1b-5b29ca1aaeee
+  RandomKey = "";
+  char msg[40];
+  for (int i = 0; i < 38; i++)
+  {
+    if (i == 8 || i == 13 || i == 18 || i == 23)
+      RandomKey += '-';
+    else
+    {
+      char randomValue = random(0, 35);
+
+      if (randomValue > 25)
+      {
+        RandomKey += char((randomValue - 26) + '0');
+      }
+      else
+        RandomKey += char(randomValue + 'a');
+    }
+  }
+  Serial.print("Here is your random string: ");
+  Serial.println(RandomKey);
+
+  return RandomKey;
 }
--- Codes-ESP-TFA/Communication Test/Wifi-Client/Wifi-Client.ino
+++ Codes-ESP-TFA/Communication Test/Wifi-Client/Wifi-Client.ino
@@ -10,24 +10,28 @@ ESP8266WiFiMulti WiFiMulti;
 #include <Arduino_JSON.h>
 
 // Replace with your network credentials
-const char* ssid = "NodeMCU";  // Enter SSID here
-const char* password = "12345678";  //Enter Password here
+const char *ssid = "NodeMCU";      // Enter SSID here
+const char *password = "12345678"; //Enter Password here
 
 //Your IP address or domain name with URL path
-const char *serverNameTemp = "http://192.168.4.1/temperature";
-const char *serverPost = "http://192.168.1.1/";
 const char *serverLogin = "http://192.168.1.1/login";
+const char *serverPostData = "http://192.168.1.1/data";
 
 String temperature;
-int LoginResponseCode;
+int ResponseCode = 0;
+char MyToken[] = "wfsnmyto-n47b-6bhc-7fam-4ltzatyixcmyg5";
+String LastApiKey = "";
 
 unsigned long previousMillis = 0;
-const long interval = 5000;
+const long interval = 20000;
+
+bool isLoggedInFlag = 0;
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println();
+  delay(30);
 
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(ssid, password);
@@ -37,10 +41,8 @@ void setup()
     Serial.print(".");
   }
   Serial.println("");
-  if (WiFiMulti.run() == WL_CONNECTED)
-    Serial.println("Connected to WiFi");
-  else
-    Serial.println("No WiFi");
+  Serial.println("Connected to WiFi");
+  isLoggedInFlag = 0;
 }
 
 void loop()
@@ -49,52 +51,79 @@ void loop()
 
   if (currentMillis - previousMillis >= interval)
   {
-    Serial.println((WiFiMulti.run() == WL_CONNECTED));
-    // Check WiFi connection status
+    Serial.println();
+    Serial.println("*******Inside Loop*******");
     if ((WiFiMulti.run() == WL_CONNECTED))
     {
-      //      postMethod = httpPOSTRequest(serverPost);
+      if (!isLoggedInFlag)
+        doLogin();
+      else
+        sendData();
 
-      int counter = 0;
-      while (LoginResponseCode == 201 || counter == 10) {
-        Serial.println("Trying to login!");
-        LoginResponseCode = httpPOSTRequest(serverLogin);
-        if (LoginResponseCode == 400 || LoginResponseCode == 401)
-        {
-          Serial.println("Please Check Authorization credentials!");
-          return;
-        }
-        counter++;
-        delay(30);
-      }
-
-      // save the last HTTP GET Request
       previousMillis = currentMillis;
     }
     else
     {
       Serial.println("WiFi Disconnected");
+      WiFiMulti.addAP(ssid, password);
+      while ((WiFiMulti.run() == WL_CONNECTED))
+      {
+        delay(500);
+        Serial.print(".");
+      }
     }
   }
 }
 
-int httpPOSTRequest(const char *serverName)
+void doLogin()
+{
+  int flag = 0;
+  ResponseCode = 0;
+  while (flag < 10)
+  {
+    Serial.println("Trying to login!");
+    ResponseCode = httpPOSTLoginRequest(serverLogin);
+    if (ResponseCode == 202)
+    {
+      isLoggedInFlag = 1;
+      flag = 11;
+    }
+    Serial.println(responseCodeToString(ResponseCode));
+    flag++;
+    delay(100);
+  }
+}
+
+void sendData()
+{
+  int flag = 0;
+  ResponseCode = 0;
+  while (flag < 10)
+  {
+    Serial.println("Sending Data!");
+    ResponseCode = httpPOSTSendData(serverPostData);
+    if (ResponseCode == 201 || ResponseCode == 407)
+    {
+      flag = 11;
+    }
+    Serial.println(responseCodeToString(ResponseCode));
+    flag++;
+    delay(100);
+  }
+}
+
+int httpPOSTSendData(const char *serverName)
 {
   WiFiClient client;
   HTTPClient http;
 
   // Your IP address with path or Domain name with URL path
   http.begin(client, serverName);
-  //  http.addHeader("Content-Type", "text/plain");
   http.addHeader("Content-Type", "application/json");
-  //  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.addHeader("Token", "asdas-asdasd-adasd-asdasd-dasd");
 
   JSONVar myObject;
-  myObject["username"] = "admin";
-  myObject["password"] = "password123";
-  myObject["Token"] = "asdas-asdasd-adasd-asdasd-dasd";
-  // JSON.stringify(myVar) can be used to convert the json var to a String
+  myObject["ApiKey"] = LastApiKey;
+  myObject["data"] = String(random(18.00, 32.00));
   String jsonString = JSON.stringify(myObject);
 
   // Send HTTP POST request
@@ -107,6 +136,9 @@ int httpPOSTRequest(const char *serverName)
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
     payload = http.getString();
+    Serial.println(payload);
+    if (httpResponseCode == 407)
+      isLoggedInFlag = 0;
   }
   else
   {
@@ -119,18 +151,23 @@ int httpPOSTRequest(const char *serverName)
   return httpResponseCode;
 }
 
-
-
-String httpGETRequest(const char *serverName)
+int httpPOSTLoginRequest(const char *serverName)
 {
   WiFiClient client;
   HTTPClient http;
 
   // Your IP address with path or Domain name with URL path
   http.begin(client, serverName);
+  http.addHeader("Content-Type", "application/json");
 
-  // Send HTTP Get request
-  int httpResponseCode = http.GET();
+  JSONVar myObject;
+  myObject["username"] = "admin";
+  myObject["password"] = "password123";
+  myObject["Token"] = MyToken;
+  String jsonString = JSON.stringify(myObject);
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(jsonString);
 
   String payload = "--";
 
@@ -139,6 +176,12 @@ String httpGETRequest(const char *serverName)
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
     payload = http.getString();
+    Serial.println(payload);
+    JSONVar incomingMessage = JSON.parse(payload);
+    payload = JSON.stringify(incomingMessage["ApiKey"]);
+    payload.remove(0, 1);
+    payload.remove(38, 1);
+    LastApiKey = payload;
   }
   else
   {
@@ -148,5 +191,94 @@ String httpGETRequest(const char *serverName)
   // Free resources
   http.end();
 
-  return payload;
+  return httpResponseCode;
+}
+
+String responseCodeToString(int code)
+{
+  switch (code)
+  {
+    case 100:
+      return F("Continue");
+    case 101:
+      return F("Switching Protocols");
+    case 200:
+      return F("OK");
+    case 201:
+      return F("Created");
+    case 202:
+      return F("Accepted");
+    case 203:
+      return F("Non-Authoritative Information");
+    case 204:
+      return F("No Content");
+    case 205:
+      return F("Reset Content");
+    case 206:
+      return F("Partial Content");
+    case 300:
+      return F("Multiple Choices");
+    case 301:
+      return F("Moved Permanently");
+    case 302:
+      return F("Found");
+    case 303:
+      return F("See Other");
+    case 304:
+      return F("Not Modified");
+    case 305:
+      return F("Use Proxy");
+    case 307:
+      return F("Temporary Redirect");
+    case 400:
+      return F("Bad Request");
+    case 401:
+      return F("Unauthorized");
+    case 402:
+      return F("Payment Required");
+    case 403:
+      return F("Forbidden");
+    case 404:
+      return F("Not Found");
+    case 405:
+      return F("Method Not Allowed");
+    case 406:
+      return F("Not Acceptable");
+    case 407:
+      return F("Proxy Authentication Required");
+    case 408:
+      return F("Request Time-out");
+    case 409:
+      return F("Conflict");
+    case 410:
+      return F("Gone");
+    case 411:
+      return F("Length Required");
+    case 412:
+      return F("Precondition Failed");
+    case 413:
+      return F("Request Entity Too Large");
+    case 414:
+      return F("Request-URI Too Large");
+    case 415:
+      return F("Unsupported Media Type");
+    case 416:
+      return F("Requested range not satisfiable");
+    case 417:
+      return F("Expectation Failed");
+    case 500:
+      return F("Internal Server Error");
+    case 501:
+      return F("Not Implemented");
+    case 502:
+      return F("Bad Gateway");
+    case 503:
+      return F("Service Unavailable");
+    case 504:
+      return F("Gateway Time-out");
+    case 505:
+      return F("HTTP Version not supported");
+    default:
+      return "Invalid Response Code";
+  }
 }
